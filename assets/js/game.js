e6ce1826e872228157cbb516697d1b1f724b0024
rafaellopes21 <rafaellopes1025@gmail.com>
2023-06-29 14:49:05 -0300

    Now, it's possible to play the basic game!

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,11 +17,16 @@ var itemMatch = false;
 var gradeGame = document.querySelector("#grade-game");
 var refreshGradeTime = false;
 var timer = document.querySelector("#clock-time");
-var maxTime = 15;
+var maxTime = 30;
 var timerClockInterval = setInterval(updatetimer, 1000);
+var score = document.querySelector("#score-numbers");
+var playerScore = 0;
+var playerScoreInSession = 0;
 var totalItems = 0;
+var totalItemsInSession = 0;
 var totalHits = 0;
 var totalErrors = 0;
+var results = document.querySelector("#results-in-game");
 var bg = ["bg-danger", "bg-primary", "bg-dark", "bg-info", "bg-success", "bg-warning", "bg-secondary"];
 
 
@@ -31,13 +36,18 @@ function sortItemGame(){
     sortedItem.insertAdjacentHTML("beforeend", '<div class="sorted-item '+itemMatch+'"></div>');
 }
 function populateGrade(gradeSizeItens = 8, refreshingTime = 5){
+    //Clear all items befor load the grade
     gradeGame.innerHTML = "";
+    totalItemsInSession = 0;
+    playerScoreInSession = 0;
+
     for (let x = 0; x < gradeSizeItens; x++) {
         for (let y = 0; y < gradeSizeItens; y++) {
             let randomBg = Math.floor(Math.random() * bg.length);
 
             if(bg[randomBg] == itemMatch){
                 totalItems++;
+                totalItemsInSession++;
             }
 
             gradeGame.insertAdjacentHTML("beforeend",
@@ -50,6 +60,7 @@ function populateGrade(gradeSizeItens = 8, refreshingTime = 5){
     //Refreshes the grade game after X seconds
     refreshGradeTime = setTimeout(function (){
         playSFX('zapsplat_multimedia_game_retro_musical_short_tone_003.mp3');
+        sumSessionPoints();
         populateGrade();
     }, refreshingTime * 1000);
 }
@@ -57,22 +68,63 @@ function populateGrade(gradeSizeItens = 8, refreshingTime = 5){
 function gradeAction(){
     document.querySelectorAll(".square-item").forEach(square => {
         square.addEventListener('click', function (){
-            destroyCorrect(square);
+            if(!square.hasAttribute("disabled"))
+                destroyCorrect(square);
         });
     });
 }
 
+function checkSessionSelectedItems(){
+    //Check if player selected all itens in session to make manual refresh
+    if(totalItemsInSession == playerScoreInSession){
+        clearTimeout(refreshGradeTime);
+        playSFX('zapsplat_multimedia_game_retro_musical_ascend_advance_correct.mp3');
+        populateGrade();
+    }
+}
+
 function destroyCorrect(e){
+    //Make some logics to prevent erros
     e.classList.remove("entry");
+    e.setAttribute("disabled", "disabled");
+
+    //If item selected is the same from the random item, then it's right
     if(itemMatch == e.getAttribute('item')){
         playSFX('zapsplat_multimedia_game_retro_musical_ascend_positive.mp3');
         e.classList.add("destroy");
+
+        //Sum the points and store.
         totalHits++;
-    } else {
+        sumScore();
+        checkSessionSelectedItems();
+    } else { //If is different, then it's wrong answer!
         playSFX('zapsplat_multimedia_game_retro_musical_incorrect_fail_negative.mp3');
         e.classList.add("wrong");
         totalErrors++;
+
+        //Remove the class from element to release the animation to do again if necessary.
+        setTimeout(function (){ e.classList.remove("wrong"); }, 1000);
     }
+
+    //Remove disable button to prevent erros by double click.
+    setTimeout(function (){ e.removeAttribute("disabled"); }, 1000);
+}
+
+function sumSessionPoints(){
+    let sumPoints = (playerScoreInSession * totalItemsInSession) / playerScoreInSession;
+
+    if(playerScoreInSession == totalItemsInSession){
+        sumPoints = (playerScoreInSession * totalItemsInSession) / 2;
+    }
+
+    sumScore((sumPoints ? sumPoints : 0));
+}
+function sumScore(addPoints = 0){
+    playerScoreInSession++;
+    let sumPoints = (totalHits / totalErrors) == "Infinity" ? (totalHits / 1) : (totalHits / totalErrors);
+    sumPoints = (sumPoints + addPoints);
+    playerScore += Math.ceil((sumPoints ? sumPoints : 0));
+    score.innerText = String(playerScore).padStart(5, "0");
 }
 
 function updatetimer() {
@@ -95,11 +147,31 @@ function updatetimer() {
     }
 
     if (maxTime < 0) {
+        sumSessionPoints();
+        saveResults();
+
         playSFX('zapsplat_multimedia_game_retro_musical_level_complete_003.mp3');
         clearAll();
+
         timer.textContent = "00:00";
-        console.table([{'total_items': totalItems, 'hits': totalHits, 'errors': totalErrors}]);
+
+        loadModal(
+            'partials/modal/index',
+            'Congratulations '+getPlayer()['player']+'!',
+            'OK',
+            'menu/main-menu',
+            'game/result/index');
     }
+}
+
+function saveResults(){
+    results.value = JSON.stringify(
+        {'total_items': totalItems, 'hits': totalHits, 'errors': totalErrors, 'score': playerScore}
+    );
+}
+
+function getResults(){
+    return JSON.parse(results.value);
 }
 
 function clearAll(){
--- assets/js/global.js
+++ assets/js/global.js
@@ -2,11 +2,20 @@ window.onload = function (){
 
 }
 
+var playerData = [];
+
 function getBaseUrl(){
     return window.location.protocol+"//"+window.location.host+(window.location.pathname).replace("index.html", "");
 }
 
+function getPlayer(){
+    return playerData;
+}
+
 function startGaming(){
+    playerData = {
+        'player': document.querySelector("#nickname").value,
+    };
     include('menu/main-menu', '#main-content');
 }
 
@@ -23,4 +32,31 @@ function enableSfxButtonClicked(){
 
 function loadView(viewPath){
     include(viewPath);
+}
+
+function loadModal(viewPath, modalTitle, btnName = 'Save', btnRedirect = false, contentPath = false){
+    //Fix path name
+    viewPath = "views/"+((viewPath.replace("views/", "").replace("/views/", "")).replace(".html", ""))+".html";
+
+    //Load the view in the element and execute some requireds functions
+    $('#modalTemplateLoad').load(viewPath, function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "error"){
+            showErrorInConsole("Error: " + xhr.status + ": " + xhr.statusText);
+        }
+        if(statusTxt == "success"){
+            enableSfxButtonClicked();
+            document.querySelector("#modalTemplateTitle").innerHTML = modalTitle;
+            document.querySelector("#modalTemplateSave").innerHTML = btnName;
+
+            if(contentPath){
+                include(contentPath, '#modalTemplateContent');
+            }
+
+            if(btnRedirect){
+                document.querySelector("#modalTemplateSave").setAttribute("onclick", "loadView('"+btnRedirect+"')");
+            }
+
+            document.querySelector("#modalTemplateBtn").click();
+        }
+    });
 }
